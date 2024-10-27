b6d9e1dfaf49384d4db3aed211c8aa5ca0f10595
KYLE\junho <junhooo1111@gmail.com>
2024-10-27 19:34:36 +0900

    연혁 페이지 추가

--- tkpage/src/NavBar/index.tsx
+++ tkpage/src/NavBar/index.tsx
@@ -7,22 +7,35 @@ import menuOpenIcon from "../icons/menu-open.webp";
 import { useRecoilState } from "recoil";
 import { isMenuOpened } from "../states/main";
 import clsx from "clsx";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export function NavBar() {
   const [isOpened, setIsOpened] = useRecoilState(isMenuOpened);
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const isNotHome = location.pathname !== "/";
+
+  console.log(isNotHome);
 
   return (
-    <div className={clsx(styles.component, isOpened && styles.opened)}>
+    <div
+      className={clsx(
+        styles.component,
+        (isOpened || isNotHome) && styles.opened
+      )}
+    >
       <button
         className={styles.menuButton}
         onClick={() => setIsOpened(!isOpened)}
       >
-        <img src={isOpened ? menuOpenIcon : menuIcon} alt="menu" />
+        <img src={isOpened || isNotHome ? menuOpenIcon : menuIcon} alt="menu" />
       </button>
       <button
-        className={clsx(styles.homeButton, isOpened && styles.opened)}
+        className={clsx(
+          styles.homeButton,
+          (isOpened || isNotHome) && styles.opened
+        )}
         onClick={() => {
           setIsOpened(false);
           navigate("/");
@@ -31,7 +44,7 @@ export function NavBar() {
         <img src={logoImage} alt="logo" />
         <img
           className={styles.title}
-          src={isOpened ? titleOpenImage : titleImage}
+          src={isOpened || isNotHome ? titleOpenImage : titleImage}
           alt="title"
         />
         <h2 className={styles.sub}>{`|주|`}</h2>

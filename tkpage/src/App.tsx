a535752ee4fb8682daed4dc8049aa6ce660d37d4
KYLE\junho <junhooo1111@gmail.com>
2024-10-27 18:33:05 +0900

    NavBar 오버레이 추가

--- tkpage/src/App.tsx
+++ tkpage/src/App.tsx
@@ -1,13 +1,49 @@
 import { Footer } from "./Footer";
 import { Main } from "./Main";
 import { NavBar } from "./NavBar";
+import { isMenuOpened } from "./states/main";
 import styles from "./style.module.scss";
+import { useRecoilValue } from "recoil";
+
+const companyInfoMenu = {
+  title: "회사소개",
+  items: ["인사말", "연혁", "오시는 길"],
+};
+
+const businessAreasMenu = {
+  title: "사업분야",
+  items: ["공기조화기", "항온항습기", "크린룸설비", "기타설비"],
+};
 
 function App() {
+  const isOpened = useRecoilValue(isMenuOpened);
+
   return (
     <div className={styles.component}>
       <NavBar />
       <Main />
+      <div className={`${styles.overlay} ${isOpened ? styles.open : ""}`}>
+        <div className={styles.menuWrapper}>
+          <div className={styles.menuContents}>
+            <h2 className={styles.main}>{companyInfoMenu.title}</h2>
+            <div className={styles.subLine} />
+            {companyInfoMenu.items.map((item) => (
+              <h2 key={item} className={styles.sub}>
+                {item}
+              </h2>
+            ))}
+          </div>
+          <div className={styles.menuContents}>
+            <h2 className={styles.main}>{businessAreasMenu.title}</h2>
+            <div className={styles.subLine} />
+            {businessAreasMenu.items.map((item) => (
+              <h2 key={item} className={styles.sub}>
+                {item}
+              </h2>
+            ))}
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
--- tkpage/src/NavBar/index.tsx
+++ tkpage/src/NavBar/index.tsx
@@ -1,17 +1,31 @@
 import styles from "./style.module.scss";
 import logoImage from "../images/logo.webp";
 import titleImage from "../images/title.webp";
+import titleOpenImage from "../images/title-open.webp";
 import menuIcon from "../icons/menu.webp";
+import menuOpenIcon from "../icons/menu-open.webp";
+import { useRecoilState } from "recoil";
+import { isMenuOpened } from "../states/main";
+import clsx from "clsx";
 
 export function NavBar() {
+  const [isOpened, setIsOpened] = useRecoilState(isMenuOpened);
+
   return (
-    <div className={styles.component}>
-      <button className={styles.menuButton}>
-        <img src={menuIcon} alt="menu" />
+    <div className={clsx(styles.component, isOpened && styles.opened)}>
+      <button
+        className={styles.menuButton}
+        onClick={() => setIsOpened(!isOpened)}
+      >
+        <img src={isOpened ? menuOpenIcon : menuIcon} alt="menu" />
       </button>
-      <button className={styles.homeButton}>
+      <button className={clsx(styles.homeButton, isOpened && styles.opened)}>
         <img src={logoImage} alt="logo" />
-        <img className={styles.title} src={titleImage} alt="title" />
+        <img
+          className={styles.title}
+          src={isOpened ? titleOpenImage : titleImage}
+          alt="title"
+        />
         <h2 className={styles.sub}>{`|주|`}</h2>
       </button>
     </div>

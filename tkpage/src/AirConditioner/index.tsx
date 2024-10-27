5d9505a5b8170c5b2c9ba41ca70fa960deb23e89
KYLE\junho <junhooo1111@gmail.com>
2024-10-27 19:48:39 +0900

    에어컨 페이지 추가

--- tkpage/src/AirConditioner/index.tsx
+++ tkpage/src/AirConditioner/index.tsx
@@ -1,5 +1,15 @@
+import { TitleComponent } from "../common/components/TitleComponent";
 import styles from "./style.module.scss";
+import mainImage from "../images/air-conditioner.webp";
 
 export function AirConditioner() {
-  return <div className={styles.component}>air conditioner</div>;
+  return (
+    <div className={styles.component}>
+      <TitleComponent
+        label="공기조화기(AHU)"
+        sub="ROOM 내부에 공기의 분배, 압력, 냉난방 및 습도 조정 등 기실 내에서 필요한 조건을 위하여 공조기를 조정하는 기능을 가진 공조설비로 실에 맞는 쾌적한 환경을 만든다."
+      />
+      <img src={mainImage} alt="mainImage" />
+    </div>
+  );
 }
--- tkpage/src/common/components/TitleComponent/index.tsx
+++ tkpage/src/common/components/TitleComponent/index.tsx
@@ -1,10 +1,11 @@
 import styles from "./style.module.scss";
 
-export function TitleComponent(props: { label: string }) {
+export function TitleComponent(props: { label: string; sub?: string }) {
   return (
     <div className={styles.component}>
       <h2>{props.label}</h2>
       <div className={styles.subLine} />
+      {props.sub !== undefined && <p className={styles.sub}>{props.sub}</p>}
     </div>
   );
 }
